0e6d581b23bddd31c39230e81a799c089ce973f3
Shivansu08 <123229614+Shivansu08@users.noreply.github.com>
2023-05-11 15:34:43 +0530

    node learn

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,2 +1,14 @@
-const fs = require("fs");
-fs.copyFileSync("file1.txt", "file2.txt");
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+
+var mysuperheroes = superheroes.random();
+
+console.log(mysuperheroes);
+
+var supervillains = require("supervillains");
+
+var mysupervillains = supervillains.random();
+
+console.log(mysupervillains);
